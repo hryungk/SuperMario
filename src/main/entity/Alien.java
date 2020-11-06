211656469bd5d803cac894db74219f4193aa2d9c
HoppyCat <38110685+hryungk@users.noreply.github.com>
2020-11-06 12:52:03 -0800

    Added unit for fire and interaction with Shot.
    Added unit parameter to take into account fire.
    Added interaction with Shot object.
    Adjusted conditionals which deal with ground level since Commons.ground now refers to the actual position of ground line.

--- src/main/entity/Alien.java
+++ src/main/entity/Alien.java
@@ -4,8 +4,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import main.SuperPusheen;
 import main.Commons;
+import main.gfx.Color;
+import main.gfx.Font;
 import main.gfx.Screen;
 import main.level.Level;
 
@@ -19,7 +20,11 @@ public class Alien extends Sprite {
     private int curDx;
     private int inity;
     private boolean activated, crushed;
+    public boolean shot;
     private int deathTime;
+    public String score;
+    private int scoreX;
+    private double scoreY;
     
     public Alien(int x, int y, Level level) {                
         super(level);
@@ -44,8 +49,11 @@ public class Alien extends Sprite {
         counter = 0;
         curDx = dx;
         
-        activated = crushed = false;
+        activated = crushed = shot =false;
         deathTime = 0;
+        score = "";
+        scoreX = 0;
+        scoreY = 0;
         
         var alienImg = "src/LittleGoomba.png";                   
        
@@ -57,6 +65,8 @@ public class Alien extends Sprite {
             String msg = String.format("No such file found: %s", ex.getMessage());
             System.out.println(msg);
         }
+        
+        unit = (int) (Math.log10(width)/Math.log10(2)); // the size of block to be used (4 for 16 px sprite and 3 for 8px sprite)
     }    
         
     // Positions the alien in horizontal direction.
@@ -64,9 +74,9 @@ public class Alien extends Sprite {
     public void tick() {
         super.tick();     
                 
-        if (deathTime >= 100)
+        if (deathTime >= 20)
             die();
-        if (crushed && !removed)
+        else if ((crushed || shot) && !removed)
             deathTime++;
         else {
             if (counter % 2 == 0) 
@@ -86,7 +96,12 @@ public class Alien extends Sprite {
     //            stopped = !move(dx, dy);            
     //        }
 
-            boolean stopped = !move(dx, dy);        
+            boolean stopped = !move(dx, dy);      
+            
+            if (y > Commons.BOARD_HEIGHT)
+                remove();
+            
+            
             int offset = level.getOffset();
             if (x <= 0)
                 hurt(health);        
@@ -110,7 +125,7 @@ public class Alien extends Sprite {
             else
                 dy = ySpeed;        
 
-            if (dy > 0  && y < Commons.GROUND && willBeGrounded()) {
+            if (dy > 0  && y + height < Commons.GROUND && willBeGrounded()) {
                 int yt1 = y + dy + ES;
                 int backoff = yt1 - (yt1 >> 4) * 16;
                 if (backoff > 1)
@@ -123,32 +138,55 @@ public class Alien extends Sprite {
             if (stopped && dx != 0) {
                 dx = - dx;
                 curDx = dx;                    
-            }
+            }                   
         }
+        // Update score location        
+        if (score.isEmpty()){
+            scoreX = x;
+            scoreY = y;
+        } else {
+            scoreY = scoreY - 0.5;
+            if (scoreY < y - ES)
+                remove();
+        }     
     }  
     
     /** What happens when the player touches an entity.
      * @param sprite The sprite that this sprite is touched by. */    
     protected void touchedBy(Sprite sprite) {
         if (sprite instanceof Player) { // if the entity touches the player
-            if (sprite.y + sprite.height <= y) {
+            boolean isOverTop = sprite.y + sprite.height <= y;
+            boolean willCrossTop = sprite.y + sprite.height + sprite.dy >= y;
+            boolean isXInRange = sprite.x + sprite.width > x && x + width > sprite.x;
+            if (isOverTop && willCrossTop && isXInRange && !crushed) {
                 xS += 2;
                 crushed = true;
                 dx = 0;
-            } else if (!crushed)
+                level.player.score += Commons.SPE; // gives the player 1000 points of score
+                score = Integer.toString(Commons.SPE);                
+                ((Player) sprite).setCrushedAlien(true);
+            } else if (!crushed && !shot)
                 sprite.hurt(1); // hurts the player, damage is based on lvl.
         }
-        if (sprite instanceof Shot) { // if the shot touches this alien
-            hurt(health); // hurts this alien.
-            sprite.hurt(sprite.health); // hurt the shot
-        }
+//        if (sprite instanceof Shot) { // if the shot touches this alien
+////            hurt(health); // hurts this alien.
+//            sprite.hurt(sprite.health); // hurt the shot
+//            if (!shot) {                
+//                shot = true;
+//                dx = 0;
+//                level.player.score += Commons.SPE; // gives the player 1000 points of score
+//                score = Integer.toString(Commons.SPE);
+//            }
+//        }
         if (sprite instanceof Alien) {
-            if (dx != 0) {
-                dx = -dx;
-                curDx = dx; 
-                sprite.dx = -sprite.dx;
-                ((Alien) sprite).curDx = sprite.dx;
-            }            
+            if (!((Alien) sprite).isCrushed()) {
+                if (dx != 0) {
+                    dx = -dx;
+                    curDx = dx; 
+                    sprite.dx = -sprite.dx;
+                    ((Alien) sprite).curDx = sprite.dx;
+                }          
+            }
         }
     }
 
@@ -160,26 +198,39 @@ public class Alien extends Sprite {
         int sw = screen.getSheet().width;   // width of sprite sheet (256)
         int colNum = sw / Commons.PPS;    // Number of squares in a row (32)    
 //        screen.render(x, y, xS + yS * colNum, flip1); // draws the top-left tile
-        int PPS = Commons.PPS;
+        int PPS = Commons.PPS;        
         
-        if (crushed) {
-            screen.render(x, y + PPS, xS + yS * colNum, 0); // render the top-left part of the sprite         
-            screen.render(x + PPS, y + PPS, (xS + 1) + yS * colNum, 0);  // render the top-right part of the sprite
-        } else {
-            screen.render(x + PPS * flip1, y, xS + yS * colNum, flip1); // render the top-left part of the sprite         
-            screen.render(x - PPS * flip1 + PPS, y, (xS + 1) + yS * colNum, flip1);  // render the top-right part of the sprite
-            screen.render(x + PPS * flip1, y + PPS, xS + (yS + 1) * colNum, flip1); // render the bottom-left part of the sprite
-            screen.render(x - PPS * flip1 + PPS, y + PPS, xS + 1 + (yS + 1) * colNum, flip1); // render the bottom-right part of the sprite        
+        if (isVisible()) {
+            if (crushed) {
+                screen.render(x, y + PPS, xS + yS * colNum, 0); // render the top-left part of the sprite         
+                screen.render(x + PPS, y + PPS, (xS + 1) + yS * colNum, 0);  // render the top-right part of the sprite
+            } else if (shot) {
+                flip1 = 0;
+                screen.render(x + PPS * flip1, y + PPS, xS + yS * colNum, 2); // render the top-left part of the sprite         
+                screen.render(x - PPS * flip1 + PPS, y + PPS, (xS + 1) + yS * colNum, 2);  // render the top-right part of the sprite
+                screen.render(x + PPS * flip1, y, xS + (yS + 1) * colNum, 2); // render the bottom-left part of the sprite
+                screen.render(x - PPS * flip1 + PPS, y, xS + 1 + (yS + 1) * colNum, 2); // render the bottom-right part of the sprite        
+            } else {
+                screen.render(x + PPS * flip1, y, xS + yS * colNum, flip1); // render the top-left part of the sprite         
+                screen.render(x - PPS * flip1 + PPS, y, (xS + 1) + yS * colNum, flip1);  // render the top-right part of the sprite
+                screen.render(x + PPS * flip1, y + PPS, xS + (yS + 1) * colNum, flip1); // render the bottom-left part of the sprite
+                screen.render(x - PPS * flip1 + PPS, y + PPS, xS + 1 + (yS + 1) * colNum, flip1); // render the bottom-right part of the sprite        
+            }
         }
-            
+
+        // Render score location once died
+        if (!score.isEmpty()){
+            Font.draw(score, screen, scoreX, (int)scoreY, Color.WHITE);
+//            System.out.println("scoreY: " + (int)scoreY);
+        }        
     }
     
     /** What happens when the alien dies */
     @Override
     public void die() {
         super.die(); // calls the die() method in Mob.java
-        if (level.player != null && y <= Commons.GROUND && x > 0) { // if the player is not null
-            level.player.score += 10; // gives the player 1000 points of score
+        if (level.player != null && y + height <= Commons.GROUND && x > 0) { // if the player is not null
+//            level.player.score += 10; // gives the player 1000 points of score
 //            level.player.gameWon(); // player wins the game
         }
     }
@@ -193,7 +244,12 @@ public class Alien extends Sprite {
     }
     
     public boolean blocks(Sprite e) {
-        if (crushed) return true;
-        else return false;
+//        if (crushed) return true;
+//        else 
+            return false;
     }   
+    
+    public boolean isCrushed() {
+        return crushed;
+    }
 }
