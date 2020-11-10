f2b1eec915df235a1a4f8922dfb82d092fa397ac
HoppyCat <38110685+hryungk@users.noreply.github.com>
2020-11-09 17:32:51 -0800

    Update tile mechanics
    Bricks and question bricks bounce up when hit by the player on the bottom.
    Question bricks become inactive once hit.

--- src/main/level/tile/QuestionBrickTile.java
+++ src/main/level/tile/QuestionBrickTile.java
@@ -15,9 +15,8 @@ import main.level.Level;
  *
  * @author HRK
  */
-public class QuestionBrickTile extends Tile {
+public class QuestionBrickTile extends InteractiveTile {
     
-    private final int ES = Commons.ENTITY_SIZE;
     private int bNum, bCounter, dbNum, scale;
     
     public QuestionBrickTile(int id) {
@@ -25,69 +24,85 @@ public class QuestionBrickTile extends Tile {
         
         xS = 10;
         yS = 0;
-        scale = 100;
-        bNum = 0;
+        scale = 7;
+        
         bCounter = tickCount;
         dbNum = 1;
+        bNum = 0;
     }
 
+    
+    /** Render method, used in sub-classes
+     * @param screen current screen
+     * @param level current level
+     * @param xt x tile position of the current level
+     * @param yt x tile position of the current level */
     @Override
-    public void render(Screen screen, Level level, int x, int y) {
-        
-        int xt = xS + bNum * 2; // animation based on walk distance (0 is standing still and 2 is moving)  
+    public void render(Screen screen, Level level, int xt, int yt) {
+        super.render(screen, level, xt, yt);
         
         int sw = screen.getSheet().width;   // width of sprite sheet (256)
         int colNum = sw / Commons.PPS;    // Number of squares in a row (32)
 //        screen.render(x * ES + 0, y * ES + 0, xS + yS * colNum, 0); // renders the top-left part of the brick        
+                
+//        if (isHitBottom) bNum = 3;  // No more blinking after hit on the bottom.
+            
+        int xSCur = xS + bNum * 2; // animation based on walk distance (0 is standing still and 2 is moving)          
         
         int PPS = Commons.PPS;
         for (int ys = 0; ys < hS; ys++) {
             for (int xs = 0; xs < wS; xs++) {
-                screen.render(x * ES + xs * PPS, y * ES + ys * PPS, (xt + xs) + (yS + ys) * colNum, 0); // Loops through all the squares to render them all on the screen.                    
+                screen.render(xt * ES + xs * PPS, y + ys * PPS, (xSCur + xs) + (yS + ys) * colNum, 0); // Loops through all the squares to render them all on the screen.                    
             }
         }
     }
 
     @Override
     public void tick(int xt, int yt, Level level) {
+        super.tick(xt, yt, level);
 //        bCounter++;        
-        
-        switch (bNum) {
-            case 0: // bright 
-                dbNum = Math.abs(dbNum);
-                if (bCounter > 2*scale) {
-                    bNum += dbNum;
-                    bCounter = 0;
-                } else
-                    bCounter++;
-                break;
-            case 1: // intermediate
-                if (bCounter > scale) {
-                    bNum += dbNum;
-                    bCounter = 0;
-                } else
-                    bCounter++;                
-                break;
-            case 2: // dark
-                if (bCounter > scale) {
-                    dbNum = - dbNum;
-                    bNum += dbNum;
-                    bCounter = 0;
-                } else
-                    bCounter++;
-                break;
+        if (!isHitBottom) {
+            switch (bNum) {
+                case 0: // bright 
+                    dbNum = Math.abs(dbNum);
+                    if (bCounter > 2*scale) {
+                        bNum += dbNum;
+                        bCounter = 0;
+                    } else
+                        bCounter++;
+                    break;
+                case 1: // intermediate
+                    if (bCounter > scale) {
+                        bNum += dbNum;
+                        bCounter = 0;
+                    } else
+                        bCounter++;                
+                    break;
+                case 2: // dark
+                    if (bCounter > scale) {
+                        dbNum = - dbNum;
+                        bNum += dbNum;
+                        bCounter = 0;
+                    } else
+                        bCounter++;
+                    break;
+            }
         }
 //        System.out.println("Block counter: " + bCounter + ", Qblock number: " + bNum);
+//        System.out.println("bNum = " + bNum);
     }
-
-    @Override
-    public void hurt(int x, int y, Sprite source, int dmg, int attackDir) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    public void hurt() {
+        if (!isHitBottom) {
+            super.hurt();
+        }
+        else
+            ds = 0;
+        bNum = 3;
     }
 
     @Override
     public void bumpedInto(int xt, int yt, Entity entity) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
