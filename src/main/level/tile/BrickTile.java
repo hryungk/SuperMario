38c209171c8cc7c2901c1e5d66dd2f177021c344
HoppyCat <38110685+hryungk@users.noreply.github.com>
2020-11-09 17:34:56 -0800

    Update tile mechanics
    Bricks and question bricks bounce up when hit by the player on the bottom.

--- src/main/level/tile/BrickTile.java
+++ src/main/level/tile/BrickTile.java
@@ -15,7 +15,7 @@ import main.level.Level;
  *
  * @author HRK
  */
-public class BrickTile extends Tile {
+public class BrickTile extends InteractiveTile {
 
     private final int ES = Commons.ENTITY_SIZE;
     
@@ -29,10 +29,12 @@ public class BrickTile extends Tile {
     /** Render method, used in sub-classes
      * @param screen current screen
      * @param level current level
-     * @param x x tile position of the current level
-     * @param y x tile position of the current level */
+     * @param xt x tile position of the current level
+     * @param yt x tile position of the current level */
     @Override
-    public void render(Screen screen, Level level, int x, int y) {        
+    public void render(Screen screen, Level level, int xt, int yt) {        
+        super.render(screen, level, xt, yt);
+        
         int sw = screen.getSheet().width;   // width of sprite sheet (256)
         int colNum = sw / Commons.PPS;    // Number of squares in a row (32)
 //        screen.render(x * ES + 0, y * ES + 0, xS + yS * colNum, 0); // renders the top-left part of the brick        
@@ -40,20 +42,16 @@ public class BrickTile extends Tile {
         int PPS = Commons.PPS;
         for (int ys = 0; ys < hS; ys++) {
             for (int xs = 0; xs < wS; xs++) {
-                screen.render(x * ES + xs * PPS, y * ES + ys * PPS, (xS + xs) + (yS + ys) * colNum, 0); // Loops through all the squares to render them all on the screen.                    
+                screen.render(xt * ES + xs * PPS, y + ys * PPS, (xS + xs) + (yS + ys) * colNum, 0); // Loops through all the squares to render them all on the screen.                    
             }
         }  
     }
 
     @Override
     public void tick(int xt, int yt, Level level) {
+        super.tick(xt, yt, level);
         if (removed)
             level.setTile(xt, yt, sky);        
-    }
-
-    @Override
-    public void hurt(int x, int y, Sprite source, int dmg, int attackDir) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
